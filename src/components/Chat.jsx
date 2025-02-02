9f17680585f83c66fa44fee7f9925f13410306d4
mritunjayYadav88 <binshuyadav123+98@gmail.com>
2025-02-02 18:41:43 +0530

    Added chat features in real time

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { createSocketConnection } from "../utils/socket";
 import { useSelector } from "react-redux";
+import axios from "axios";
+import BASE_URL from "../utils/constants";
 
 export const Chat = () => {
   const { targetUserId } = useParams();
@@ -10,6 +12,25 @@ export const Chat = () => {
   const [newMessage, setNewMessage] = useState("");
   const user = useSelector((store) => store.user);
   const userId = user?._id;
+
+  const fetchChatMessages = async () => {
+    const chat = await axios.get(BASE_URL + "/chat/" + targetUserId, {
+      withCredentials: true,
+    });
+    console.log(chat.data.messages);
+    const chatMessages = chat?.data?.messages.map((msg) => {
+      return {
+        firstName: msg.senderId.firstName,
+        lastName: msg.senderId.lastName,
+        text: msg.text,
+      };
+    });
+    setMessages(chatMessages);
+  };
+
+  useEffect(() => {
+    fetchChatMessages();
+  }, []);
 
   useEffect(() => {
     if (!userId) {
@@ -22,10 +43,13 @@ export const Chat = () => {
       userId,
       targetUserId,
     });
-    socket.on("messageReceived", ({ firstName, newMessage }) => {
+    socket.on("messageReceived", ({ firstName, lastName, newMessage }) => {
       console.log("Message received");
       // console.log(firstName + " " + newMessage);
-      setMessages((messages) => [...messages, { firstName, newMessage }]);
+      setMessages((messages) => [
+        ...messages,
+        { firstName, lastName, text: newMessage },
+      ]);
     });
     //it will be called when the chat component is unmounted
     return () => {
@@ -37,6 +61,7 @@ export const Chat = () => {
     const socket = createSocketConnection();
     socket.emit("sendMessage", {
       firstName: user.firstName,
+      lastName: user.lastName,
       userId,
       targetUserId,
       newMessage,
@@ -49,7 +74,13 @@ export const Chat = () => {
       <div className="flex-1 overflow-scroll p-5">
         {messages.map((msg, index) => {
           return (
-            <div key={index} className="chat chat-start">
+            <div
+              key={index}
+              className={
+                "chat " +
+                (user.firstName === msg.firstName ? "chat-start" : "chat-end")
+              }
+            >
               <div className="chat-image avatar">
                 <div className="w-10 rounded-full">
                   <img
@@ -59,10 +90,10 @@ export const Chat = () => {
                 </div>
               </div>
               <div className="chat-header">
-                {msg.firstName}
+                {`${msg.firstName} ${msg.lastName}`}
                 <time className="text-xs opacity-50">12:45</time>
               </div>
-              <div className="chat-bubble">{msg.newMessage}</div>
+              <div className="chat-bubble">{msg.text}</div>
               <div className="chat-footer opacity-50">Delivered</div>
             </div>
           );
